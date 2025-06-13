df563daaef8422fadd8a7891b4c233538d983fd3
Darmin Reyes <119169294+Darmin-RC@users.noreply.github.com>
2025-06-12 23:18:30 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 });)
